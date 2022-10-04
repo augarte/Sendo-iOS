cdf7875dcd89f13afff107b2dc50a2c2d2d17dbe
Aimar Ugarte <ugarteaimar@gmail.com>
2022-10-04 23:02:27 +0200

    Added date range buttons on SimpleLineChart

--- Sendo/View/HomeTabBar/Tabs/ProgressView/SimpleLineChart/SimpleLineChart.swift
+++ Sendo/View/HomeTabBar/Tabs/ProgressView/SimpleLineChart/SimpleLineChart.swift
@@ -10,8 +10,8 @@ import UIKit
 private enum Constants {
     static let cornerRadiusSize = CGSize(width: 8.0, height: 8.0)
     static let margin: CGFloat = 20.0
-    static let topBorder: CGFloat = 60
-    static let bottomBorder: CGFloat = 50
+    static let topBorder: CGFloat = 20.0
+    static let bottomBorder: CGFloat = 65.0
     static let colorAlpha: CGFloat = 0.3
     static let circleDiameter: CGFloat = 5.0
 }
@@ -29,7 +29,10 @@ class SimpleLineChart: UIView {
     @IBInspectable var gradientStartColor: UIColor = UIColor.hexStringToUIColor(hex: "FEB775")
     @IBInspectable var gradientEndColor: UIColor = UIColor.hexStringToUIColor(hex: "FD4345")
     
+    var periodSelectors: Array<(String, Int)> = [("1 Month", 2629800), ("3 Month", 7889400), ("1 Year", 31557600), ("All Time", 3155760000)]
+    var selectedPeriod: (String, Int)?
     var graphPoints: Array<(Int, Double)> = []
+    var filteredGraphPoints: Array<(Int, Double)> = []
 
     override func draw(_ rect: CGRect) {
         let width = rect.width
@@ -73,12 +76,12 @@ class SimpleLineChart: UIView {
         // Margins
         let margin = Constants.margin
         let graphWidth = width - margin * 2 - 4
-        let xValues = graphPoints.map({ point in return point.0 })
+        let xValues = filteredGraphPoints.map({ point in return point.0 })
         guard let minXValue = xValues.min() else { return }
         guard let maxXValue = xValues.max() else { return }
         let columnXPoint = { (column: Int) -> CGFloat in
             // Calculate the gap between points
-            guard self.graphPoints.count > 1 else {
+            guard self.filteredGraphPoints.count > 1 else {
                 return CGFloat(graphWidth/2) + margin + 2
             }
             let xPoint = CGFloat(column-minXValue) / CGFloat(maxXValue-minXValue) * graphWidth
@@ -88,11 +91,11 @@ class SimpleLineChart: UIView {
         let topBorder = Constants.topBorder
         let bottomBorder = Constants.bottomBorder
         let graphHeight = height - topBorder - bottomBorder
-        let yValues = graphPoints.map({ point in return point.1 })
+        let yValues = filteredGraphPoints.map({ point in return point.1 })
         guard let minYValue = yValues.min() else { return }
         guard let maxYValue = yValues.max() else { return }
         let columnYPoint = { (graphPoint: Double) -> CGFloat in
-            guard self.graphPoints.count > 1 else {
+            guard self.filteredGraphPoints.count > 1 else {
                 return graphHeight + topBorder - (graphHeight/2)
             }
             let yPoint = CGFloat(graphPoint-minYValue) / CGFloat(maxYValue-minYValue) * graphHeight
@@ -105,12 +108,12 @@ class SimpleLineChart: UIView {
         UIColor.white.setStroke()
             
         let graphPath = UIBezierPath()
-        graphPath.move(to: CGPoint(x: columnXPoint(graphPoints[0].0), y: columnYPoint(graphPoints[0].1)))
+        graphPath.move(to: CGPoint(x: columnXPoint(filteredGraphPoints[0].0), y: columnYPoint(filteredGraphPoints[0].1)))
             
-        // Add points for each item in the graphPoints array
+        // Add points for each item in the filteredGraphPoints array
         // at the correct (x, y) for the point
-        for i in 1..<graphPoints.count {
-            let nextPoint = CGPoint(x: columnXPoint(graphPoints[i].0), y: columnYPoint(graphPoints[i].1))
+        for i in 1..<filteredGraphPoints.count {
+            let nextPoint = CGPoint(x: columnXPoint(filteredGraphPoints[i].0), y: columnYPoint(filteredGraphPoints[i].1))
             graphPath.addLine(to: nextPoint)
         }
         
@@ -124,9 +127,9 @@ class SimpleLineChart: UIView {
             }
             
             clippingPath.addLine(to: CGPoint(
-                x: columnXPoint(graphPoints[graphPoints.count - 1].0),
+                x: columnXPoint(filteredGraphPoints[filteredGraphPoints.count - 1].0),
                 y: height))
-            clippingPath.addLine(to: CGPoint(x: columnXPoint(graphPoints[0].0), y: height))
+            clippingPath.addLine(to: CGPoint(x: columnXPoint(filteredGraphPoints[0].0), y: height))
             clippingPath.close()
             clippingPath.addClip()
                 
@@ -147,8 +150,8 @@ class SimpleLineChart: UIView {
         
         // -------------------
         // Data points
-        for i in 0..<graphPoints.count {
-            var point = CGPoint(x: columnXPoint(graphPoints[i].0), y: columnYPoint(graphPoints[i].1))
+        for i in 0..<filteredGraphPoints.count {
+            var point = CGPoint(x: columnXPoint(filteredGraphPoints[i].0), y: columnYPoint(filteredGraphPoints[i].1))
             point.x -= Constants.circleDiameter / 2
             point.y -= Constants.circleDiameter / 2
               
@@ -162,10 +165,93 @@ class SimpleLineChart: UIView {
             )
             circle.fill()
         }
+        
+        // -------------------
+        // Date selectors
+        addCollectionView(width: width, height: height)
+//        selectedPeriod = periodSelectors[0]
+//        addPeriodSelectors(width: width, height: height)
+    }
+    
+    func addCollectionView(width: Double, height: Double) {
+        let margin = Constants.margin
+        let collectionWidth = width - (margin * 2)
+        let collectionHeight = Constants.bottomBorder - (margin * 2)
+        
+        let collectionView = UICollectionView(frame: CGRect(
+            origin: CGPoint(x: margin,
+                            y: height - collectionHeight - margin),
+            size: CGSize(width: collectionWidth,
+                         height: collectionHeight)
+        ), collectionViewLayout: .init())
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        collectionView.setCollectionViewLayout(layout, animated: true)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        //collectionView.register(UINib(nibName: "PeriodSelectorCell", bundle: nil), forCellWithReuseIdentifier: "PeriodSelectorCell")
+        collectionView.register(UINib(nibName: "PeriodSelectorCell", bundle: nil), forCellWithReuseIdentifier:"PeriodSelectorCell")
+
+        addSubview(collectionView)
+    }
+    
+    func addPeriodSelectors(width: Double, height: Double) {
+        //guard let periods = periodSelectors else { return }
+        let periods = periodSelectors
+        
+        let margin = Constants.margin
+        let buttonWidth = ((width - (margin * Double(periods.count+1))) / Double(periods.count))
+        let buttonHeight = Constants.bottomBorder - margin
+        
+        for i in 0..<periods.count {
+            let isSelected = (selectedPeriod != nil) && selectedPeriod! == periods[i]
+            let separation = i != 0 ? margin : 0
+            let button = UIButton(frame: CGRect(
+                origin: CGPoint(x: margin + ((separation + buttonWidth) * Double(i)),
+                                y: height - buttonHeight - (margin / 2)),
+                size: CGSize(width: buttonWidth,
+                             height: buttonHeight)
+            ))
+            button.tag = i
+            button.setTitle(periods[i].0, for: .normal)
+            button.titleLabel?.textAlignment = .center
+            button.backgroundColor =  isSelected ? gradientStartColor.withAlphaComponent(0.1) : .white.withAlphaComponent(0.1)
+            button.layer.borderWidth = 1
+            button.layer.borderColor = isSelected ? gradientStartColor.cgColor : UIColor.white.cgColor
+            button.layer.cornerRadius = buttonHeight / 2
+            button.titleLabel?.font = button.titleLabel?.font.withSize(12)
+            button.addTarget(self, action: #selector(changeDateRange), for: .touchUpInside)
+            addSubview(button)
+        }
+    }
+    
+    @objc func changeDateRange(sender: UIButton) {
+        let timestamp = Int(NSDate().timeIntervalSince1970)
+        selectedPeriod = periodSelectors[sender.tag]
+        filteredGraphPoints = graphPoints.filter({ value in
+            return value.0 > timestamp - (selectedPeriod?.1 ?? timestamp)
+        })
+        self.setNeedsDisplay()
     }
     
     open func setPoints(points: Array<(Int, Double)>) {
         graphPoints = points
+        filteredGraphPoints = graphPoints
         self.setNeedsDisplay()
+    }
+}
+
+extension SimpleLineChart: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return periodSelectors.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let periodSelector = periodSelectors[indexPath.row]
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PeriodSelectorCell", for: indexPath) as! PeriodSelectorCell
+        cell.configureCell(period: periodSelector)
+        return cell
     }
 }
